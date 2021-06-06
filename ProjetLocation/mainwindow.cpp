2dab22f28f41c453988d98cd7d7db5b2fe715ce6
mohamed HJ <83469444+mohamed-HJ@users.noreply.github.com>
2021-06-06 23:58:36 +0100

    Add files via upload

---
+++ ProjetLocation/mainwindow.cpp
@@ -0,0 +1,135 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_pushButton_10_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_pushButton_12_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_pushButton_17_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_pushButton_14_clicked()
+{
+      ui->stackedWidget->setCurrentIndex(4);
+}
+
+
+void MainWindow::on_pushButton_18_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(5);
+}
+
+
+void MainWindow::on_pushButton_24_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_pushButton_20_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(6);
+}
+
+
+void MainWindow::on_pushButton_26_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(5);
+}
+
+
+void MainWindow::on_pushButton_22_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(7);
+}
+
+
+void MainWindow::on_pushButton_28_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(5);
+}
+
+
+void MainWindow::on_pushButton_6_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(5);
+}
+
+
+void MainWindow::on_pushButton_7_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(6);
+}
+
+
+void MainWindow::on_pushButton_8_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(7);
+}
+
+
+void MainWindow::on_pushButton_9_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_pushButton_13_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(4);
+}
+
+
+void MainWindow::on_pushButton_16_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(4);
+}
+
